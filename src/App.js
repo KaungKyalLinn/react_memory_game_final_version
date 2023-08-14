61bf8a722eda709b8fa5d5fe6eb68491ad53ea1f
KaungKyalLinn <www.kaungkyallinn2015@gmail.com>
2023-08-14 15:12:22 +0630

    shuffle card and render to the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import back from "./img/back.jpg";
+import book from "./img/book.jpg";
+import bottle from "./img/bottle.jpg";
+import gold_bag from "./img/gold_bag.jpg";
+import paper from "./img/paper.jpg";
+import shield from "./img/shield.jpg";
+import sword from "./img/sword.jpg";
+
+const card_images = [
+  {"src" : book},
+  {"src" : bottle},
+  {"src" : gold_bag},
+  {"src" : paper},
+  {"src" : shield},
+  {"src" : sword},
+]
 
 function App() {
+
+  const [cards,set_cards] = useState([]);
+  const [turn,set_turn] = useState(0);
+
+  const shuffle_cards = () => {
+    const shuffled = [...card_images, ...card_images]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => (
+      {...card , id : Math.random()}
+    ));
+
+    set_cards(shuffled);
+  }
+
+  console.log(cards , turn);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="heading">Magic Cards</h1>
+        <button className="start_btn" onClick={shuffle_cards}>New game</button>
+
+        <div className="card_bord">
+
+
+          {cards.map((card)=>
+            (
+              <div className="the_card" key={card.id}>
+                <img src={card.src} alt="" className="front" />
+                <img src="../public/img/back.jpg" alt="" className="back" />
+              </div>
+            )
+          )}
+        </div>
+
+      </div>
     </div>
   );
 }
